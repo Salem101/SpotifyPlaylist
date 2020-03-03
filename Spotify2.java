61e17b227190ca5fa5c4aa690ad9f7538826ec63
Salem101 <55041326+Salem101@users.noreply.github.com>
2020-03-03 16:47:36 -0500

    Create Spotify2.java

---
+++ Spotify2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Spotify2 {
+	public static void main(String[] args) throws Exception {
+		// We need to provide file path as the parameter:
+		// double backquote is to avoid compiler interpret words
+		// like \test as \t (ie. as a escape sequence)
+		File file1 = new File("C:\\Users\\rivka\\OneDrive\\Documents\\CodeFiles\\Stats1.csv");
+		File file2 = new File("C:\\Users\\rivka\\eclipse-workspace\\Rivka 2019\\src\\A2\\Stats2.csv");
+
+		ArrayList<String> tracks = new ArrayList<String>();
+
+		ArrayList<File> myFiles = new ArrayList<File>();
+		myFiles.add(file1);
+		myFiles.add(file2);
+		for (int i = 0; i < myFiles.size(); i++) {
+			BufferedReader br = new BufferedReader(new FileReader(myFiles.get(i)));
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] info = line.split(",");
+
+				String tr = info[1];
+				tracks.add(tr);
+
+			}
+			br.close();
+		}
+		//System.out.println(tracks);
+		Queue <String> playlist = new LinkedList<String>();
+		//loop for counting
+		int counter =1;
+		for(int j=0; j<tracks.size(); j++) {
+			playlist.add(tracks.get(j));
+			System.out.println(counter + tracks.get(j));
+			counter++;
+		}
+
+		String head = playlist.peek(); 
+		System.out.println("Last played song: " +head);
+		
+		//method to get song
+	}
+}
